ee418d02e6ceebb971c37dbe563964a0ce84eea9
Victor GALLOIS-WONG <vgallois@e2r2p6.42.fr>
2019-10-19 11:13:59 +0200

    mapbonus

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,20 +6,26 @@
 /*   By: vgallois <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 18:18:05 by vgallois          #+#    #+#             */
-/*   Updated: 2019/10/17 07:58:39 by vgallois         ###   ########.fr       */
+/*   Updated: 2019/10/19 11:12:32 by vgallois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *))
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void(*del)(void*))
 {
 	t_list	*new;
+	t_list	*tmp;
 
 	new = NULL;
 	while (f && lst)
 	{
-		ft_lstadd_back(&new, f(lst->content));
+		if (!(tmp = f(lst->content)))
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, tmp);
 		lst = lst->next;
 	}
 	return (new);
